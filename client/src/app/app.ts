1de631dd19c0cf58bbad761e1906d06b43d628e1
uday5t61 <uday5t61@gmail.com>
2025-06-23 17:28:28 +0530

    Added header

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
+  baseUrl = 'http://localhost:5001/api';
   protected title = 'client';
+  productList: Product[] = [];
+
+  private httpClient = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.httpClient
+      .get<Pagination<Product>>(this.baseUrl + '/products')
+      .subscribe({
+        next: (response) => {
+          this.productList = response.data;
+        },
+        error: (error: any) => {
+          console.error('Error fetching version:', error);
+          this.title = 'Error fetching version';
+        },
+        complete: () => {
+          console.log('Product fetch complete');
+        },
+      });
+  }
 }
---
+++ client/src/app/layout/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { MatBadge } from '@angular/material/badge';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIcon, MatButton, MatBadge],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {}
